0fdc6182b1b5a2c4092ceb231ce461256eabb15d
Alex Glushko <aglushko2@slb.com>
2024-10-06 17:18:27 +0300

    Add get all files request

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 from .main import router as listner_router
 
 app = FastAPI()
+add_pagination(app)
 app.include_router(listner_router)
 
 
--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ import io
 import uuid
 import json
 from datetime import datetime
-from typing import Annotated
 from fastapi import APIRouter, Header, Response
+from fastapi_pagination import Page, paginate
+from typing import Annotated
 from pydantic import BaseModel
 from minio import Minio
 from minio.error import S3Error
@@ -82,7 +83,7 @@ async def post_data(
 
 @router.get(
     "/phonendo/phonendo-record-get",
-    tags=["Listner"],
+    tags=["Accesser"],
     responses={
         500: {"description": "Internal error"},
         400: {"description": "Faild to fiend data"},
@@ -105,6 +106,40 @@ async def get_data(
         obj = client.get_object(bucket_name, f"{id}.json")
         data = json.loads(obj.data.decode())
         return PhonendoData.model_validate(data)
+    except S3Error as e:
+        print("Error:", e)
+        return Response(status_code=400)
+
+
+@router.get(
+    "/phonendo/phonendo-record-getall",
+    tags=["Accesser"],
+    responses={
+        500: {"description": "Internal error"},
+        400: {"description": "Faild to fiend data"},
+        401: {"description": "Authentification error"},
+        200: {"description": "Successfully save file", "model": Page[PhonendoData]},
+    },
+    response_model=Page[PhonendoData],
+)
+async def getall_data(
+    X_API_Key: Annotated[str | None, Header()] = None,
+):
+    if X_API_Key != XAPIKey:
+        return Response(status_code=401)
+    try:
+        client = Minio(
+            "minio:9000", access_key=access_key, secret_key=secret_key, secure=False
+        )
+        bucket_name = "phonendo-data"
+        objects = client.list_objects(bucket_name)
+        files = []
+        for obj in objects:
+            if obj.object_name:
+                res = client.get_object(bucket_name, obj.object_name)
+                data = json.loads(res.data.decode())
+                files.append(PhonendoData.model_validate(data))
+        return paginate(files)
     except S3Error as e:
         print("Error:", e)
         return Response(status_code=400)
